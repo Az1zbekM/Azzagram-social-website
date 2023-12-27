6521c59c6913dca8f1e9840461a753c1cc08f9fd
Az1zbekM <azizbekmamatqulov8001@gmal.com>
2023-12-27 11:00:46 +0500

    reels issue => fixed

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -71,7 +71,7 @@ export const useGetPosts = () => {
 			const lastId = lastPage.documents[lastPage.documents.length - 1].$id
 			return lastId
 		},
-		initialPageParam: true,
+		initialPageParam: false,
 	})
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,3 @@
-// import { type ClassValue, clsx } from "clsx"
-// import { twMerge } from "tailwind-merge"
- 
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs))
-// }
-
-///new
-
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
