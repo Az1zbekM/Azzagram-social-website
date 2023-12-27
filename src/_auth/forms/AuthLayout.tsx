10b132b2b4c7621cdc9b2e5f555b42b9e07b557c
Az1zbekM <azizbekmamatqulov8001@gmal.com>
2023-12-28 00:51:36 +0500

    message

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom'
 
 
 const AuthLayout = () => {
-	const isAuthenticated =	localStorage.getItem('isAuthenticated') ;
+	const isAuthenticated = false;
 	return (
 		<>
 			{isAuthenticated ? (
