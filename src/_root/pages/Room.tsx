c7def2f329a7ee5f7506784851485e661f797338
Azizbek <azizbekmamatqulov8001@gmail.com>
2024-02-02 01:48:15 +0500

    bug is fixed

--- src/_root/pages/Room.tsx
+++ src/_root/pages/Room.tsx
@@ -4,9 +4,9 @@ import { appwriteConfig, client, databases } from '@/lib/appwrite/config'
 import { ID, Permission, Query, Role } from 'appwrite'
 import { useEffect, useState } from 'react'
 
- interface Payload {
-		$id: string 
- }
+interface Payload {
+    $id: string
+}
 
 //  
 interface Message {
@@ -28,25 +28,25 @@ interface Message {
 }
 
 const Room = (): JSX.Element => {
-    
-    const {user} = useUserContext()
 
-    const [messages, setMessages] = useState<Message[] | null>(null) 
+    const { user } = useUserContext()
+
+    const [messages, setMessages] = useState<Message[] | null>(null)
     const [messageBody, setMessageBody] = useState<string>("")
-    
+
 
     useEffect(() => {
         getMessages()
-        
-      const unsubscribe =  client.subscribe(
-           `databases.${appwriteConfig.databaseId}.collections.${appwriteConfig.messagesCollectionId}.documents`, 
-            response => {   
-                console.log("SUBSCRIPTION", response)    
+
+        const unsubscribe = client.subscribe(
+            `databases.${appwriteConfig.databaseId}.collections.${appwriteConfig.messagesCollectionId}.documents`,
+            response => {
+                console.log("SUBSCRIPTION", response)
 
                 if (response.events.includes("databases.*.collections.*.documents.*.create")) {
-                     console.log("MESSAGE CREATED")  
-                     setMessages(prevState => [response.payload as Message, ...(prevState || [])])
-                }   
+                    console.log("MESSAGE CREATED")
+                    setMessages(prevState => [response.payload as Message, ...(prevState || [])])
+                }
                 if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
                     console.log("MESSAGE DELETED")
                     setMessages(prevState => (prevState || []).filter(message => message.$id !== (response.payload as Payload).$id))
@@ -55,9 +55,9 @@ const Room = (): JSX.Element => {
 
             })
 
-                return () => {
-                    unsubscribe()
-                }
+        return () => {
+            unsubscribe()
+        }
     }, [])
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -70,7 +70,7 @@ const Room = (): JSX.Element => {
         }
 
         let permissions = [
-        Permission.write(Role.users(user?.$id as string)),
+            Permission.write(Role.users(user?.$id as string)),
         ]
 
         let response = await databases.createDocument<Message>(
@@ -81,7 +81,7 @@ const Room = (): JSX.Element => {
             permissions
         )
 
-        
+
         console.log("CREATED", response)
         // setMessages(prevState => [response, ...prevState])
         setMessageBody("")
@@ -93,11 +93,20 @@ const Room = (): JSX.Element => {
                 appwriteConfig.databaseId,
                 appwriteConfig.messagesCollectionId,
                 [Query.orderDesc('$createdAt')]
-            )
+            );
             console.log("RESPONSE", response)
-
-            setMessages(response.documents)
-
+            const messages = response.documents.map(doc => ({
+                username: doc.username,
+                $createdAt: doc.$createdAt,
+                $id: doc.$id,
+                Body: doc.Body,
+                $databaseId: doc.$databaseId,
+                $collectionId: doc.$collectionId,
+                userId: doc.userId,
+                $updatedAt: doc.$updatedAt,
+                $permissions: doc.$permissions,
+            }))
+            setMessages(messages)
         } catch (error) {
             console.log("error", error)
         }
@@ -119,66 +128,66 @@ const Room = (): JSX.Element => {
         }
     }
     return (
-			<main className='max-w-[600px] mx-auto my-5'>
-				<TestHeader />
-				<div className='room p-[2em] border-2 border-primary-500 rounded bg-dark-3'>
-					<form onSubmit={handleSubmit} id='message--form'>
-						<div>
-							<textarea
-								required
-								maxLength={1000}
-								placeholder='Message'
-								onChange={e => setMessageBody(e.target.value)}
-								value={messageBody}
-								className='bg-dark-3 w-full rounded border border-primary-500 p-2 text-sm text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2'
-							></textarea>
-							{/*input submit */}
-							<div className='mt-0'>
-								<input
-									className='mb-2 w-full rounded border border-primary-500 p-2 text-sm text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2'
-									type='submit'
-									value={'Send'}
-									id='submit'
-								/>
-							</div>
-						</div>
-					</form>
-					<div>
-						{messages?.map(message => (
-							<div key={message.$id} className='flex flex-col gap-2 mb-2'>
-								<div className='flex justify-between items-center'>
-									<p>
-										{message?.username ? (
-											<span>{message.username}</span>
-										) : (
-											<span>Anonymous User</span>
-										)}
-									</p>
+        <main className='max-w-[600px] mx-auto my-5'>
+            <TestHeader />
+            <div className='room p-[2em] border-2 border-primary-500 rounded bg-dark-3'>
+                <form onSubmit={handleSubmit} id='message--form'>
+                    <div>
+                        <textarea
+                            required
+                            maxLength={1000}
+                            placeholder='Message'
+                            onChange={e => setMessageBody(e.target.value)}
+                            value={messageBody}
+                            className='bg-dark-3 w-full rounded border border-primary-500 p-2 text-sm text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2'
+                        ></textarea>
+                        {/*input submit */}
+                        <div className='mt-0'>
+                            <input
+                                className='mb-2 w-full rounded border border-primary-500 p-2 text-sm text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2'
+                                type='submit'
+                                value={'Send'}
+                                id='submit'
+                            />
+                        </div>
+                    </div>
+                </form>
+                <div>
+                    {messages?.map(message => (
+                        <div key={message.$id} className='flex flex-col gap-2 mb-2'>
+                            <div className='flex justify-between items-center'>
+                                <p>
+                                    {message?.username ? (
+                                        <span>{message.username}</span>
+                                    ) : (
+                                        <span>Anonymous User</span>
+                                    )}
+                                </p>
 
-									<p className='text-gray-500 small-regular'>
-										{/* create date but only display time */}
-										{new Date(message.$createdAt).toLocaleTimeString()}
-									</p>
+                                <p className='text-gray-500 small-regular'>
+                                    {/* create date but only display time */}
+                                    {new Date(message.$createdAt).toLocaleTimeString()}
+                                </p>
 
-									{message.$permissions.includes(`delete(\"users\")`) && (
-										<button onClick={() => deleteMessage(message.$id)}>
-											<img src='/assets/icons/trash.svg' alt='delete' />
-										</button>
-									)}
-									{/* <button onClick={() => deleteMessage(message.$id)}>
+                                {message.$permissions.includes(`delete(\"users\")`) && (
+                                    <button onClick={() => deleteMessage(message.$id)}>
+                                        <img src='/assets/icons/trash.svg' alt='delete' />
+                                    </button>
+                                )}
+                                {/* <button onClick={() => deleteMessage(message.$id)}>
 										<img src='/assets/icons/trash.svg' alt='delete' />
 									</button> */}
-								</div>
+                            </div>
 
-								<div className=' w-fit max-w-full break-words border border-primary-500 rounded p-2'>
-									<span className='text-primary-500'>{message.Body}</span>
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-			</main>
-		)
+                            <div className=' w-fit max-w-full break-words border border-primary-500 rounded p-2'>
+                                <span className='text-primary-500'>{message.Body}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Room
